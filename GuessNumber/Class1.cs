a66909b0de93dcd997f247b87fdc3d76843a81c6
w62829 <w62829@student.wsiz.edu.pl>
2020-11-06 23:27:53 +0600

    Add project files.

---
+++ GuessNumber/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessNumber
+{
+    public static class Random1
+    {
+        public static int Newnum(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+    public static class Game
+    {
+        public static int[] Guess(int input)
+        {
+            while (true)
+            {
+                int randno = Random1.Newnum(1, 101);
+                int count = 1;
+                while (true)
+                {
+                    Console.Write("Enter a number between 1 and 100(0 to quit):");
+                 
+                    if (input == 0)
+                        break;
+                    else if (input < randno)
+                    {
+                        Console.WriteLine("Too small.");
+                        count++;
+                        continue;
+                    }
+                    else if (input > randno)
+                    {
+                        Console.WriteLine("Too big.");
+                        count++;
+                        continue;
+                    }
+                    else if (input == randno)
+                    {
+                        Console.WriteLine($"You won! The number was {randno}!");
+
+                        Console.WriteLine("It took you {0} {1}.\n", count,
+                                           count == 1 ? "try" : "tries");
+                        break;
+                    }
+                }
+            }
+        }            
+             
+    }
+}
---
+++ GuessNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name for player1");
+            var player1 = Console.ReadLine();
+            Console.WriteLine("Enter name for player2");
+            var player2 = Console.ReadLine();
+            var temp = player1;
+            while(true)
+            {
+                
+                if(temp == player1)
+                {
+                    var i = Convert.ToInt32(Console.ReadLine());
+                    Game.Guess(i);
+                    temp = player2;
+                    continue;
+                }
+                if(temp == player2)
+                {
+                    var k = Convert.ToInt32(Console.ReadLine());
+                    Game.Guess(k);
+                    temp = player1;
+                    continue;
+                }    
+                
+
+                
+            }
+             
+
+        }
+    }
+    
+}
